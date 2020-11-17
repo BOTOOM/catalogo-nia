3fa83e59083f1f23f8576e4ae343bd889a463937
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-11-17 02:25:28 -0500

    uso de lazy loading

--- src/app/modules/catalago/catalago-routing.module.ts
+++ src/app/modules/catalago/catalago-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VistaCatalogoComponent } from './components/vista-catalogo/vista-catalogo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VistaCatalogoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
